48cfd9e720d1b0fe104a44c20fb20712a7d1147f
yanrong.loy@gmail.com <lynne@linux.local>
2013-10-10 23:08:14 +0800

    update branch master

---
+++ js/index.js
@@ -0,0 +1,155 @@
+var PIANO_KEYS = 28;
+
+$(document).ready(function() {
+
+    $(".key").bind('mousedown', function () {
+        var currentKey = $(this);
+        currentKey.removeClass('pressed').addClass('pressed');
+        play_multi_sound('tone-' + currentKey.attr('id'));
+    })
+
+    $(".key").bind('mouseup', function () {
+        $(this).removeClass('pressed');
+    })
+})
+
+
+//ARRAY WITH ALL THE KEYS
+//the array content starts from element 1 so eleemnt 0 i zero, empty, nada, 0 gree
+var keyboardKeys = [];
+var i = 0;
+
+for (; i < PIANO_KEYS; ) {
+    keyboardKeys.push(eval('pKey' + (i++)));
+}
+
+i = 0;
+//LOOP trought all the  keyboard-piano keys
+for (; i < PIANO_KEYS; ) {
+
+    //BIND ON KEY DOWN
+    $(document).bind('keydown', keyboardKeys[i], function (evt){
+
+        //console.log("obajdam se ot funkciqta vutre " + evt.data.value +  evt.data.flag + evt.data.sound);
+
+        //check the flag false - key is down, true - key is up
+        if(evt.data.flag) {
+            evt.data.flag = false;
+            $(evt.data.value).addClass('pressed');
+            play_multi_sound(evt.data.sound);
+        }
+
+        return false;
+    });
+
+
+    //$(document).bind('keydown', 'r',function (evt){ alert('adasd'); });
+
+    //BIND ON KEY UP
+    $(document).bind('keyup', keyboardKeys[i], function (evt){
+
+        //console.log("obajdame se ot funkciqta vutre " + evt.data.value +  evt.data.flag + evt.data.sound);
+
+        //check the flag false - key is down, true - key is up
+        evt.data.flag = true;
+        $(evt.data.value).removeClass('pressed');
+        //stop_multi_sound(evt.data.sound); //don't so cool as shoud
+
+        return false; 
+
+    });
+    
+    i++;
+}
+
+var channel_max = 32,										// number of channels
+    audiochannels = [];
+
+i = 0;
+for (; i < channel_max; ) {									// prepare the channels
+    audiochannels[i] = [];
+    audiochannels[i]['channel'] = new Audio();						// create a new audio object
+    audiochannels[i]['finished'] = -1;							// expected end time for this channel
+    audiochannels[i]['keyvalue'] = '';
+    i ++
+}
+
+
+//PLAY SOUND
+function play_multi_sound(s) {
+
+    i = 0;
+    for (; i < channel_max; ) {
+        var now = new Date();
+        if (audiochannels[i]['finished'] < now.getTime()) {			// is this channel finished?
+
+            //console.log(audiochannels[a]['keyvalue']);
+
+            try
+            {		
+                audiochannels[i]['finished'] = now.getTime() + document.getElementById(s).duration*1000;
+                audiochannels[i]['channel'] = document.getElementById(s);
+                audiochannels[i]['channel'].currentTime = 0;
+                audiochannels[i]['channel'].volume=1;
+                audiochannels[i]['channel'].play();
+                audiochannels[i]['keyvalue'] = s;
+
+                //console.log("the key is pressed - play");
+
+                //console.log(audiochannels[a]['keyvalue']);
+
+            }
+            catch(v)
+            {	
+                //show the error message (alert or log) or hide it when public
+                //alert(v.message);
+                //	console.log(v.message); 
+            }
+
+            break;
+        }
+        i++;
+    }
+}
+
+
+function stop_multi_sound(s, sender) {
+
+    i = 0;
+    for (; i < channel_max; ) {
+
+        //console.log('chanel keyvalue = '+audiochannels[a]['keyvalue']);
+
+        if (audiochannels[i]['keyvalue'] == s) {			// is this channel finished?
+
+            try
+            {
+                audiochannels[i]['channel'] = document.getElementById(s);
+
+                //audiochannels[a]['channel'].currentTime =  audiochannels[a]['channel'].duration;
+                //audiochannels[a]['keyvalue'] = 'nema';
+
+                if(sender != undefined && sender == 'mouse') {
+                    setTimeout ("audiochannels[i]['channel'].pause()", 2500 );
+                    setTimeout ("audiochannels[i]['channel'].currentTime = 0", 2500 );
+                }else {
+                    //audiochannels[a]['channel'].volume=0;
+                    setTimeout ("audiochannels[i]['channel'].pause()", 2500 );
+                    setTimeout ("audiochannels[i]['channel'].currentTime = 0", 2500 );
+                }
+
+                //console.log("the key is UP - stop sound " + s + ' = ' + audiochannels[a]['channel'].duration + '== ' + audiochannels[a]['channel'].currentTime);
+                //console.log(audiochannels[a]['keyvalue']);
+            }
+            catch(v)
+            {	
+                //show the error message (alert or log) or hide it when public
+                //alert(v.message);
+                console.log(v.message); 
+            }
+
+            break;
+        }
+        i++;
+    }
+}
---
+++ js/keyObjects.js
@@ -0,0 +1,195 @@
+pKey0 = {
+    kbKey   : 'z',
+    flag    : true,
+    value   : '#1A',
+    sound   : 'tone-1A'
+}
+
+pKey1 = {
+    kbKey   : 's',
+    flag    : true,
+    value   : '#1As',
+    sound   : 'tone-1As'
+}
+
+pKey2 = {
+    kbKey   : 'x',
+    flag    : true,
+    value   : '#1B',
+    sound   : 'tone-1B'
+}
+
+pKey3 = {
+    kbKey   : 'c',
+    flag    : true,
+    value   : '#2C',
+    sound   : 'tone-2C'
+}
+
+pKey4 = {
+    kbKey   : 'f',
+    flag    : true,
+    value   : '#2Cs',
+    sound   : 'tone-2Cs'
+}
+
+pKey5 = {
+    kbKey   : 'v',
+    flag    : true,
+    value   : '#2D',
+    sound   : 'tone-2D'
+}
+
+pKey6 = {
+    kbKey   : 'g',
+    flag    : true,
+    value   : '#2Ds',
+    sound   : 'tone-2Ds'
+}
+
+pKey7 = {
+    kbKey   : 'b',
+    flag    : true,
+    value   : '#2E',
+    sound   : 'tone-2E'
+}
+
+pKey8 = {
+    kbKey   : 'n',
+    flag    : true,
+    value   : '#2F',
+    sound   : 'tone-2F'
+}
+
+pKey9 = {
+    kbKey   : 'j',
+    flag    : true,
+    value   : '#2Fs',
+    sound   : 'tone-2Fs'
+}
+
+pKey10 = {
+    kbKey   : 'm',
+    flag    : true,
+    value   : '#2G',
+    sound   : 'tone-2G'
+}
+
+pKey11 = {
+    kbKey   : 'k 1',
+    flag    : true,
+    value   : '#2Gs',
+    sound   : 'tone-2Gs'
+}
+
+pKey12 = {
+    kbKey   : ', q',
+    flag    : true,
+    value   : '#2A',
+    sound   : 'tone-2A'
+}
+
+pKey13 = {
+    kbKey   : 'l 2',
+    flag    : true,
+    value   : '#2As',
+    sound   : 'tone-2As'
+}
+
+pKey14 = {
+    kbKey   : '. w',
+    flag    : true,
+    value   : '#2B',
+    sound   : 'tone-2B'
+}
+
+pKey15 = {
+    kbKey   : 'e',
+    flag    : true,
+    value   : '#3C',
+    sound   : 'tone-3C'
+}
+
+pKey16 = {
+    kbKey   : '4',
+    flag    : true,
+    value   : '#3Cs',
+    sound   : 'tone-3Cs'
+}
+
+pKey17 = {
+    kbKey   : 'r',
+    flag    : true,
+    value   : '#3D',
+    sound   : 'tone-3D'
+}
+
+pKey18 = {
+    kbKey   : '5',
+    flag    : true,
+    value   : '#3Ds',
+    sound   : 'tone-3Ds'
+}
+
+pKey19 = {
+    kbKey   : 't',
+    flag    : true,
+    value   : '#3E',
+    sound   : 'tone-3E'
+}
+
+pKey20 = {
+    kbKey   : 'y',
+    flag    : true,
+    value   : '#3F',
+    sound   : 'tone-3F'
+}
+
+pKey21 = {
+    kbKey   : '7',
+    flag    : true,
+    value   : '#3Fs',
+    sound   : 'tone-3Fs'
+}
+
+pKey22 = {
+    kbKey   : 'u',
+    flag    : true,
+    value   : '#3G',
+    sound   : 'tone-3G'
+}
+
+pKey23 = {
+    kbKey   : '8',
+    flag    : true,
+    value   : '#3Gs',
+    sound   : 'tone-3Gs'
+}
+
+pKey24 = {
+    kbKey   : 'i',
+    flag    : true,
+    value   : '#3A',
+    sound   : 'tone-3A'
+}
+
+pKey25 = {
+    kbKey   : '9',
+    flag    : true,
+    value   : '#3As',
+    sound   : 'tone-3As'
+}
+
+pKey26 = {
+    kbKey   : 'o',
+    flag    : true,
+    value   : '#3B',
+    sound   : 'tone-3B'
+}
+
+pKey27 = {
+    kbKey   : 'p',
+    flag    : true,
+    value   : '#4C',
+    sound   : 'tone-4C'
+}
