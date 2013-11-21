8f8140fb761329e4c1e8d05073e9276b74292509
yanrong.loy@gmail.com <lynne@linux.local>
2013-11-22 00:26:13 +0800

    提交主干

---
+++ js/keyObjects.js
@@ -0,0 +1,195 @@
+pKey0 = {
+    kbKey   : 'z',
+    flag    : true,
+    value   : '#A1',
+    sound   : 'tone-A1'
+}
+
+pKey1 = {
+    kbKey   : 's',
+    flag    : true,
+    value   : '#As1',
+    sound   : 'tone-As1'
+}
+
+pKey2 = {
+    kbKey   : 'x',
+    flag    : true,
+    value   : '#B1',
+    sound   : 'tone-B1'
+}
+
+pKey3 = {
+    kbKey   : 'c',
+    flag    : true,
+    value   : '#C2',
+    sound   : 'tone-C2'
+}
+
+pKey4 = {
+    kbKey   : 'f',
+    flag    : true,
+    value   : '#Cs2',
+    sound   : 'tone-Cs2'
+}
+
+pKey5 = {
+    kbKey   : 'v',
+    flag    : true,
+    value   : '#D2',
+    sound   : 'tone-D2'
+}
+
+pKey6 = {
+    kbKey   : 'g',
+    flag    : true,
+    value   : '#Ds2',
+    sound   : 'tone-Ds2'
+}
+
+pKey7 = {
+    kbKey   : 'b',
+    flag    : true,
+    value   : '#E2',
+    sound   : 'tone-E2'
+}
+
+pKey8 = {
+    kbKey   : 'n',
+    flag    : true,
+    value   : '#F2',
+    sound   : 'tone-F2'
+}
+
+pKey9 = {
+    kbKey   : 'j',
+    flag    : true,
+    value   : '#Fs2',
+    sound   : 'tone-Fs2'
+}
+
+pKey10 = {
+    kbKey   : 'm',
+    flag    : true,
+    value   : '#G2',
+    sound   : 'tone-G2'
+}
+
+pKey11 = {
+    kbKey   : 'k 1',
+    flag    : true,
+    value   : '#Gs2',
+    sound   : 'tone-Gs2'
+}
+
+pKey12 = {
+    kbKey   : ', q',
+    flag    : true,
+    value   : '#A2',
+    sound   : 'tone-A2'
+}
+
+pKey13 = {
+    kbKey   : 'l 2',
+    flag    : true,
+    value   : '#As2',
+    sound   : 'tone-As2'
+}
+
+pKey14 = {
+    kbKey   : '. w',
+    flag    : true,
+    value   : '#B2',
+    sound   : 'tone-B2'
+}
+
+pKey15 = {
+    kbKey   : 'e',
+    flag    : true,
+    value   : '#C3',
+    sound   : 'tone-C3'
+}
+
+pKey16 = {
+    kbKey   : '4',
+    flag    : true,
+    value   : '#Cs3',
+    sound   : 'tone-Cs3'
+}
+
+pKey17 = {
+    kbKey   : 'r',
+    flag    : true,
+    value   : '#D3',
+    sound   : 'tone-D3'
+}
+
+pKey18 = {
+    kbKey   : '5',
+    flag    : true,
+    value   : '#Ds3',
+    sound   : 'tone-Ds3'
+}
+
+pKey19 = {
+    kbKey   : 't',
+    flag    : true,
+    value   : '#E3',
+    sound   : 'tone-E3'
+}
+
+pKey20 = {
+    kbKey   : 'y',
+    flag    : true,
+    value   : '#F3',
+    sound   : 'tone-F3'
+}
+
+pKey21 = {
+    kbKey   : '7',
+    flag    : true,
+    value   : '#Fs3',
+    sound   : 'tone-Fs3'
+}
+
+pKey22 = {
+    kbKey   : 'u',
+    flag    : true,
+    value   : '#G3',
+    sound   : 'tone-G3'
+}
+
+pKey23 = {
+    kbKey   : '8',
+    flag    : true,
+    value   : '#Gs3',
+    sound   : 'tone-Gs3'
+}
+
+pKey24 = {
+    kbKey   : 'i',
+    flag    : true,
+    value   : '#A3',
+    sound   : 'tone-A3'
+}
+
+pKey25 = {
+    kbKey   : '9',
+    flag    : true,
+    value   : '#As3',
+    sound   : 'tone-As3'
+}
+
+pKey26 = {
+    kbKey   : 'o',
+    flag    : true,
+    value   : '#B3',
+    sound   : 'tone-B3'
+}
+
+pKey27 = {
+    kbKey   : 'p',
+    flag    : true,
+    value   : '#C4',
+    sound   : 'tone-C4'
+}
